4ead9902aa70fa18f3f2cf5ce70408ece1893452
Scott Magdalein <scottmagdalein@gmail.com>
2013-01-27 22:38:57 -0500

    once again

--- myapp.rb
+++ myapp.rb
@@ -8,6 +8,8 @@ get '/' do
 end
 
 post '/template' do
+	@one = params["one"]
+	@three = params["three"]
 
 	options = {
 		:to => "scottmagdalein@gmail.com",
@@ -27,10 +29,7 @@ post '/template' do
 		}
 	}
 
-	@one = params["one"]
-	@three = params["three"]
-	haml :template
-
 	Pony.mail(options)
 
+	haml :template
 end
